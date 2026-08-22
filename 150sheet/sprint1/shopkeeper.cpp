36c34689bea7abb68c410896bfe542dc52ed4158
Dhruv Pandey <76779907+pandeydhruv2001@users.noreply.github.com>
2026-08-23 00:25:30 +0530

    Arithmetic operations
    Added calculations for sum, difference, product, and quotient of two numbers. Also included value swapping functionality.

--- 150sheet/sprint1/shopkeeper.cpp
+++ 150sheet/sprint1/shopkeeper.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
-cout<<"Enter a and b";
+int a,b;
+cout<<"Enter a and b: ";
 cin>>a>>b;
-cout<<"Sum"<<a+b;
+cout<<"Sum: "<<a+b;
+cout<<"Difference: "<<a-b<<endl;
+cout<<"Product: "<<a*b;
+cout<<"Quotient: "<<a/b;
+
+int temp = a;
+a = b;
+b = temp;
+cout<<"Swapped Values"<<a<<b;
+}
