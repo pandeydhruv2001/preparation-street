642c1767fac6096c93606cff0c58fb392c393bab
Dhruv Pandey <76779907+pandeydhruv2001@users.noreply.github.com>
2026-08-23 13:29:08 +0530

    Create divisors.cpp

---
+++ 150sheet/sprint1/divisors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>  // Needed for sort()
+
+using namespace std;
+
+void divisors(int n) {
+    vector<int> list;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            list.push_back(i);
+            if (i != n / i) {
+                list.push_back(n / i);
+            }
+        }
+    }
+    sort(list.begin(), list.end());
+
+    cout << "Divisors of " << n << " are: ";
+    for (int d : list) {
+        cout << d << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    divisors(n);  // Printing happens inside the function
+}
