b9b038b512630a50e437bc8477cde056439077f0
Dhruv Pandey <76779907+pandeydhruv2001@users.noreply.github.com>
2026-08-23 09:28:40 +0530

    Refactor sum calculation with while loop
    Added a while loop to calculate the sum of natural numbers and improved formatting.

--- 150sheet/sprint1/sumofnnaturalnumbers.cpp
+++ 150sheet/sprint1/sumofnnaturalnumbers.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main() {
 
-int N = 100;
-int sum = 0;
+	int N = 100;
+	int sum = 0;
 
-for(int i=0;i<=N;i++){
-    sum=sum+i;
-}
+	for(int i=0; i<=N; i++) {
+		sum=sum+i;
+	}
 
-cout<<"Sum using Loops: "<<sum<<endl;
-cout<<"Sum using Gauss Method: "<<N*(N+1)/2<<endl;
+	int i = 0;
+	int whilesum=0;
+	while(N+1>i) {
+		whilesum = i+whilesum;
+		i++;
+	}
+
+
+	cout<<"Sum using For Loop: "<<sum<<endl;
+	cout<<"Sum using While Loop: "<<whilesum<<endl;
+	cout<<"Sum using Gauss Method: "<<N*(N+1)/2<<endl;
 }
