35d455a95c7e0f028b5399554adc336f4687289a
Dhruv Pandey <76779907+pandeydhruv2001@users.noreply.github.com>
2026-08-23 09:11:44 +0530

    Implement value swapping and arithmetic operations
    Added swapping logic using both temporary variable and arithmetic method.

--- 150sheet/sprint1/shopkeeper.cpp
+++ 150sheet/sprint1/shopkeeper.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+
+//swapping by temp
+int temp = a;
+a = b;
+b = temp;
+cout<<"Swapped Values"<<a<<b;
+  
+//arithmetic 
 int a,b;
 cout<<"Enter a and b: ";
 cin>>a>>b;
@@ -10,8 +18,10 @@ cout<<"Difference: "<<a-b<<endl;
 cout<<"Product: "<<a*b;
 cout<<"Quotient: "<<a/b;
 
-int temp = a;
-a = b;
-b = temp;
-cout<<"Swapped Values"<<a<<b;
+//swapping by arithmetic
+a=a+b;
+b=a-b;
+a=a-b;
+
+cout<<"Swapped Values by Arithmetic"<<a<<b;
 }
