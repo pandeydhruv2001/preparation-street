e9d61c022aef7d0e1cfda58f41c22a1e0c98cced
Dhruv Pandey <76779907+pandeydhruv2001@users.noreply.github.com>
2026-09-07 10:00:06 +0530

    Add multiplication table program in 25.cpp

---
+++ 150sheet/sprint1/25.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using  namespace std;
+int main()
+{
+	cout<<"Enter the Number: ";
+	int A;
+	cin>>A;
+	for(int i=1;i<11;i++){
+	    cout<<A<<" x "<<i<<" = "<<A*i<<endl;
+	}
+	return 0;
+}
